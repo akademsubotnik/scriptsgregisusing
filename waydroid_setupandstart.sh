57fc2d41b82f7a5981dfbe5060061b5bcfe42685
Greg <44036625+gregg00@users.noreply.github.com>
2021-12-15 15:28:51 +0200

    Update waydroid_setupandstart.sh

--- waydroid_setupandstart.sh
+++ waydroid_setupandstart.sh
@@ -1,6 +1,7 @@
 #https://wiki.archlinux.org/title/Waydroid
 #Install waydroid
 ##Check system architecture/distrobution and proceed accordingly
+###Include AUR webpages on NIXOS laptop 
 sudo waydroid init #initialize waydroid
 systemctl enable --now waydroid-container.service # Start and configure waydroid to start at boot
 $ waydroid session start
